28d280045e7846ffdb7c4939baa184f5b70851aa
JULIAN KEDZIORA <j.kedziora.20@abdn.ac.uk>
2021-11-04 14:10:26 +0000

    Edited pom and app.java

--- swingStory/src/main/java/uk/ac/abdn/comp/csd/App.java
+++ swingStory/src/main/java/uk/ac/abdn/comp/csd/App.java
@@ -1,5 +1,5 @@
 package uk.ac.abdn.comp.csd;
-
+import com.github.javafaker.*;
 /**
  * Hello world!
  *
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
         System.out.println( "Hello World!" );
+        System.out.println( "Hello " + name + " " + firstName + " " + lastName );
+
     }
 }
